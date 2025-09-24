ef1fe6490689afae9ec7a1cabf463bde774925df
LeksoG <Gagloevi.lekso@gmail.com>
2025-09-24 21:31:52 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Actual configuration - DO NOT COMMIT THIS FILE
+window.CONFIG = {
+    GOOGLE_CLIENT_ID: '706092852156-7n7geb46sf0qcqumbk53anb7gl2u6uth.apps.googleusercontent.com',
+    GOOGLE_API_KEY: 'AIzaSyCWGfdMc7ly5C97VNhPWPAHsaGq9fGKCes',
+    GEMINI_API_KEY: 'AIzaSyCVYNL_SjMbNl5fFPZrlbT3HGNya7IoLfY',
+    EMAILJS_SERVICE_ID: 'service_qpxfn0h',
+    EMAILJS_TEMPLATE_ID: 'template_4wa1r6s',
+    EMAILJS_PUBLIC_KEY: 'waYmChwv7LiRd1geI'
+};
